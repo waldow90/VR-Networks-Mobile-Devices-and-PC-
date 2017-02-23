de3679cde4e5486f14b6871f5e757814b599d6b4
0sama2310 <osaiworo@gmail.com>
2017-02-22 23:36:06 -0500

    Bi Network Connection Loop
    The BiNetworkConnections.cs script is a connection loop between two
    devices a mobile and a computer, being that in unity3d without using the
    Network Manager System, the client cannot make changes throughout the
    server. So this script is like a ping poog game between the two devices
    only that the ball is the connection. Note that the script is not
    complete yet.
    The MobilexPCServer.cs script is a code that would find the two devices
    and start the connection loop.

---
+++ BiNetworkConnection.cs
@@ -0,0 +1,366 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiNetworkConnection : MonoBehaviour {
+    public string ComputerIp = "192.168.1.156";
+    public string MobileDeviceIP = "192.168.1.168";
+    public int portNUmber1 = 25001;
+    public int dNotation1 = 0;
+    public int dNotation2 = 0;
+    public int dNotation3 = 0;
+    public int dNotation4 = 0;
+
+    public bool commutingStarted = false;
+    public bool MobileTransmittion = false;
+    public bool ComputerTransmittion = false;
+
+
+    private void Update()
+    {
+        //dNotation1 = int.Parse(ComputerIp.Substring(0, 3)); 
+        if(commutingStarted && !MobileTransmittion) 
+        {
+            if(Application.platform == RuntimePlatform.Android)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                ComputerTransmittion = false;
+                commutingStarted = false;
+                MobileTransmittion = true;
+            }
+        }
+        else if (commutingStarted && !ComputerTransmittion)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                MobileTransmittion = false;
+                commutingStarted = false;
+                ComputerTransmittion = true;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        //dNotation1 = int.Parse(ComputerIp.Substring(0, 3)); 
+        if (commutingStarted && !MobileTransmittion)
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                ComputerTransmittion = false;
+                commutingStarted = false;
+                MobileTransmittion = true;
+            }
+        }
+        else if (commutingStarted && !ComputerTransmittion)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                MobileTransmittion = false;
+                commutingStarted = false;
+                ComputerTransmittion = true;
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        //dNotation1 = int.Parse(ComputerIp.Substring(0, 3)); 
+        if (commutingStarted && !MobileTransmittion)
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                ComputerTransmittion = false;
+                commutingStarted = false;
+                MobileTransmittion = true;
+            }
+        }
+        else if (commutingStarted && !ComputerTransmittion)
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                Network.InitializeServer(4, portNUmber1, true);
+                MobileTransmittion = false;
+                commutingStarted = false;
+                ComputerTransmittion = true;
+            }
+        }
+    }
+
+    private void Test()
+    {//1.
+        if((ComputerIp.Substring(1, 1)) == ".")
+        {
+            dNotation1 = System.Int32.Parse(ComputerIp.Substring(0, 0));
+            //1.1.
+            if((ComputerIp.Substring(3, 3)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(2, 2));
+                //1.1.1.
+                if ((ComputerIp.Substring(5, 5)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(4, 4));
+                    //1.1.1.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(6, 8));
+                }
+                //1.1.11.
+                else if ((ComputerIp.Substring(6, 6)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(4, 5));
+                    //1.1.11.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(7, 9));
+                }
+                //1.1.111.
+                else if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(4, 6));
+                    //1.1.111.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(8, 10));
+                }
+            }//1.11.
+            else if ((ComputerIp.Substring(4, 4)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(2, 3));
+                //1.11.1.
+                if ((ComputerIp.Substring(6, 6)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 5));
+                    //1.11.1.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(7, 9));
+                }
+                //1.11.11.
+                else if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 6));
+                    //1.11.11.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(8, 10));
+                }
+                //1.11.111.
+                else if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 7));
+                    //1.11.111.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(9, 11));
+                }
+            }//1.111.
+            else if ((ComputerIp.Substring(5, 5)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(2, 4));
+                //1.111.1.
+                if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 6));
+                    //1.111.1.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(8, 10));
+                }
+                //1.111.11.
+                else if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 7));
+                    //1.111.11.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(9, 11));
+                }
+                //1.111.111.
+                else if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 8));
+                    //1.111.111.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(10, 12));
+                }
+            }
+        }//11.
+        else if ((ComputerIp.Substring(2, 2)) == ".")
+        {
+            dNotation1 = System.Int32.Parse(ComputerIp.Substring(0, 1));
+            //11.1.
+            if ((ComputerIp.Substring(4, 4)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(3, 3));
+                //11.1.1.
+                if((ComputerIp.Substring(6, 6)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 5));
+                    //11.1.1.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(7, 9));
+                }
+                //11.1.11.
+                else if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 6));
+                    //11.1.11.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(8, 10));
+                }
+                //11.1.111.
+                else if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(5, 7));
+                    //11.1.111.111
+                    dNotation4 = System.Int32.Parse(ComputerIp.Substring(9, 11));
+                }
+            }//11.11.
+            else if ((ComputerIp.Substring(5, 5)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(3, 4));
+                //11.11.1.
+                if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 6));
+                }
+                //11.11.11.
+                else if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 7));
+                }
+                //11.11.111.
+                else if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 8));
+                }
+            }
+            //11.111.
+            else if ((ComputerIp.Substring(6, 6)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(3, 5));
+                //11.111.1.
+                if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 7));
+                }
+                //11.111.11.
+                else if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 8));
+                }
+                //11.111.111.
+                else if ((ComputerIp.Substring(10, 10)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 9));
+                }
+            }
+        }//111.
+        else if ((ComputerIp.Substring(3, 3)) == ".")
+        {
+            dNotation1 = System.Int32.Parse(ComputerIp.Substring(0, 2));
+            //111.1.
+            if ((ComputerIp.Substring(5, 5)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(4, 4)); 
+                //111.1.1.
+                if ((ComputerIp.Substring(7, 7)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 6));
+                }
+                //111.1.11.
+                else if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 7));
+                }
+                //111.1.111.
+                else if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(6, 8));
+                }
+            }
+            //111.11.
+            else if ((ComputerIp.Substring(6, 6)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(4, 5));
+                //111.11.1.
+                if ((ComputerIp.Substring(8, 8)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 7));
+                }
+                //111.11.11.
+                else if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 8));
+                }
+                //111.11.111.
+                else if ((ComputerIp.Substring(10, 10)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(7, 9));
+                }
+            }
+            //111.111.
+            else if ((ComputerIp.Substring(7, 7)) == ".")
+            {
+                dNotation2 = System.Int32.Parse(ComputerIp.Substring(4, 6));
+                //111.111.1.
+                if ((ComputerIp.Substring(9, 9)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(8, 8));
+                }
+                //111.111.11.
+                else if ((ComputerIp.Substring(10, 10)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(8, 9));
+                }
+                //111.111.111.
+                else if ((ComputerIp.Substring(11, 11)) == ".")
+                {
+                    dNotation3 = System.Int32.Parse(ComputerIp.Substring(8, 10));
+                }
+            }
+        }
+    }
+
+    private void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (player.ipAddress == ComputerIp)
+            {
+                Debug.Log("Mobile device" + ComputerIp + "has recived transmittion and is now transmitting");
+                Network.Connect(ComputerIp, portNUmber1);
+                Debug.Log("Computer have recived transmittion");
+            }
+        }
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if (player.ipAddress == MobileDeviceIP)
+            {
+                Debug.Log("Mobile device" + MobileDeviceIP + "has recived transmittion and is now transmitting");
+                Network.Connect(MobileDeviceIP, portNUmber1);
+                Debug.Log("Computer have recived transmittion");
+            }
+        }
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        bool StartSquence = false;
+        bool MTransmittion = false;
+        bool CTransmittion = false;
+
+        if (stream.isWriting)
+        {
+            CTransmittion = ComputerTransmittion;
+            stream.Serialize(ref CTransmittion);
+            MTransmittion = MobileTransmittion;
+            stream.Serialize(ref MTransmittion);
+            StartSquence = commutingStarted;
+            stream.Serialize(ref StartSquence);
+        }
+        else
+        {
+            stream.Serialize(ref CTransmittion);
+            ComputerTransmittion = CTransmittion;
+            stream.Serialize(ref MTransmittion);
+            MobileTransmittion = MTransmittion;
+            stream.Serialize(ref StartSquence);
+            commutingStarted = StartSquence;
+        }
+    }
+
+    private void OnConnectedToServer()
+    {
+        Debug.Log("Recived");
+    }
+
+    private void OnServerInitialized()
+    {
+        Debug.Log("Transmitted");
+    }
+}
---
+++ MobilexPCServer.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MobilexPCServer : MonoBehaviour {
+
+    public GameObject playerPrefab;
+    public Transform spawnObject;
+
+    public string gameName = "";
+    private bool refreshing = false;
+    private HostData[] hostData;
+    internal bool Hosting = false;
+
+    //Buttons
+    public Button Search;
+    public Button Server1;
+    public Button Server2;
+    public Button Server3;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            PairSearch.SetActive(true);
+        }
+        Button SearchButton = Search.GetComponent<Button>();
+        SearchButton.onClick.AddListener(refreshHostList);
+    }
+    void ButtonUI()
+    {
+        if (!Network.isClient && !Network.isServer) {
+            try
+            {
+                for (var i = 0; i < hostData.Length; i++)
+                {
+                    if(hostData.Length == 1)
+                    {
+                        Server1.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[0].gameName;
+                        Button Device1 = Server1.GetComponent<Button>();
+                        Device1.onClick.AddListener(ConnectDevice1);
+
+                        Server2.gameObject.SetActive(false);
+                        Server3.gameObject.SetActive(false);
+                    }
+                    else if (hostData.Length == 2)
+                    {
+                        Server1.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[0].gameName;
+                        Button Device1 = Server1.GetComponent<Button>();
+                        Device1.onClick.AddListener(ConnectDevice1);
+
+                        Server2.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[1].gameName;
+                        Button Device2 = Server2.GetComponent<Button>();
+                        Device2.onClick.AddListener(ConnectDevice2);
+
+                        Server3.gameObject.SetActive(false);
+                    }
+                    else if (hostData.Length == 1)
+                    {
+                        Server1.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[0].gameName;
+                        Button Device1 = Server1.GetComponent<Button>();
+                        Device1.onClick.AddListener(ConnectDevice1);
+
+                        Server2.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[1].gameName;
+                        Button Device2 = Server2.GetComponent<Button>();
+                        Device2.onClick.AddListener(ConnectDevice2);
+
+                        Server3.gameObject.SetActive(true);
+                        Server1.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "Device: " + hostData[2].gameName;
+                        Button Device3 = Server3.GetComponent<Button>();
+                        Device3.onClick.AddListener(ConnectDevice3);
+                    }
+                    else if(hostData.Length == 0)
+                    {
+                        Server1.gameObject.SetActive(false);
+                        Server2.gameObject.SetActive(false);
+                        Server3.gameObject.SetActive(false);
+                    }
+                }
+            }
+            catch(System.NullReferenceException ex)
+            {
+
+            }
+        }
+        else
+            Server1.gameObject.SetActive(false);
+            Server2.gameObject.SetActive(false);
+            Server3.gameObject.SetActive(false);
+    }
+
+    void ConnectDevice1()
+    {
+                Network.Connect(hostData[0]);
+    }
+    void ConnectDevice2()
+    {
+        Network.Connect(hostData[1]);
+    }
+    void ConnectDevice3()
+    {
+        Network.Connect(hostData[2]);
+    }
+
+    public GameObject DeviceSearch;
+    public GameObject DevicePaired;
+    public GameObject PairSearch;
+    void Update()
+    {
+        gameName = "level1";
+        if (refreshing)
+        {
+            if (MasterServer.PollHostList().Length > 0)
+            {
+                refreshing = false;
+                Debug.Log(MasterServer.PollHostList().Length);
+                hostData = MasterServer.PollHostList();
+            }
+        }
+        if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXEditor) {
+            if (!Network.isClient && !Network.isServer)
+            {
+                startServer();
+            }
+        }
+        else if(Application.platform == RuntimePlatform.Android)
+        {
+            ButtonUI();
+        }
+    }
+    void startServer()
+    {
+        if (!Hosting)
+        {
+            bool useNat = !Network.HavePublicAddress();
+            Network.InitializeServer(1, 25001, useNat);
+            MasterServer.RegisterHost(gameName,SystemInfo.deviceName, "This server is only made by PC/Mac not mobile devices");
+            GameObject.Find("Bi NET").GetComponent<BiNetworkConnection>().ComputerTransmittion = true;
+            Hosting = true;
+        }
+    }
+    void OnServerInitialized()
+    {
+        Debug.Log("server initialized");
+    }
+
+    private int playerCount = 0;
+    private void OnPlayerConnected(NetworkPlayer player)
+    {
+        DeviceSearch.SetActive(false);
+        Debug.Log("Player " + playerCount++ + " connected from " + player.ipAddress + ":" + player.port);
+        GameObject.Find("Bi NET").GetComponent<BiNetworkConnection>().MobileDeviceIP = player.ipAddress;
+        GameObject.Find("Bi NET").GetComponent<BiNetworkConnection>().commutingStarted = true;
+        DevicePaired.SetActive(true);
+    }
+
+    void OnConnectedToServer()
+    {
+        PairSearch.SetActive(false);
+        DeviceSearch.SetActive(false);
+        DevicePaired.SetActive(true);
+
+    }
+
+    void spawnPlayer()
+    {
+        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.OSXPlayer ||
+            Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
+        {
+            Network.Instantiate(playerPrefab, spawnObject.position, Quaternion.identity, 0).name = SystemInfo.deviceName;
+        }
+        else if(Application.platform == RuntimePlatform.Android)
+        {
+            Network.Instantiate(playerPrefab, spawnObject.position, Quaternion.identity, 0).name = SystemInfo.deviceName;
+        }
+    }
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.RegistrationSucceeded)
+            Debug.Log("Server registered");
+    }
+    void refreshHostList()
+    {
+        MasterServer.RequestHostList(gameName);
+        refreshing = true;
+    }
+}
